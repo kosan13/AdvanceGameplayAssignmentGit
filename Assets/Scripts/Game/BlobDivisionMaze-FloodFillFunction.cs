7f31a9fce10490dd9cb79b4b9d5529b6a44afcdd
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-02-12 20:00:28 +0100

    fixs error

--- Assets/Scripts/Game/BlobDivisionMaze-FloodFillFunction.cs
+++ Assets/Scripts/Game/BlobDivisionMaze-FloodFillFunction.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TileSystem.Tile_Class;
+using TileSystem.TileMap_Class;
 using Unity.VisualScripting;
 using UnityEngine;
 using static TileSystem.TileSystemFunctions;
@@ -52,18 +53,13 @@ namespace Game
                 }
                 foreach ((Tile, Tile) tiles in listToRemove) wall.Remove(tiles);
             }
-
-            foreach (var VARIABLE in gWalls)
-            {
-                
-            }
             
             foreach (HashSet<(Tile, Tile)> wallList in gWalls)
             {
                 foreach ((Tile, Tile) wall in wallList)
                 { 
                     Vector3 position = Vector3.Lerp(wall.Item1.GetWorldPosition, wall.Item2.GetWorldPosition, 0.5f);
-                    Transform temp = Instantiate(wall.Item1.Visual);
+                    Transform temp = Instantiate(wall.Item1.GetVisual);
                     position.y += 8.2f;
                     temp.localScale = Vector3.one * 0.25f; 
                     temp.position = position;
@@ -83,8 +79,8 @@ namespace Game
             // search / iteration
             while (open.Item1.Count > 0 || open.Item2.Count > 0)
             {
-                NeighborsSearchData searchData1 = new (open.Item1, closed.Item1, closed.Item2, open, closed);
-                NeighborsSearchData searchData2 = new (open.Item2, closed.Item2, closed.Item1, open, closed);
+                NeighborsSearchData searchData1 = new (open.Item1, closed.Item1, open, closed);
+                NeighborsSearchData searchData2 = new (open.Item2, closed.Item2, open, closed);
                 if (flipper)
                 {
                     if (open.Item1.Count > 0) SearchNeighbors(searchData1, RandomTolerance);
@@ -112,20 +108,20 @@ namespace Game
                 SetRegionIDOnTiles(recursiveItems);
             }
             
-            newRecursiveFloodFillData.Item1 = NewRecursiveFloodFillData(recursiveFloodFillData, recursiveFloodFillData.RecursiveItems.Item1);
-            newRecursiveFloodFillData.Item2 = NewRecursiveFloodFillData(recursiveFloodFillData, recursiveFloodFillData.RecursiveItems.Item2);
+            newRecursiveFloodFillData.Item1 = NewRecursiveFloodFillData(_tileMap, recursiveFloodFillData, recursiveFloodFillData.RecursiveItems.Item1);
+            newRecursiveFloodFillData.Item2 = NewRecursiveFloodFillData(_tileMap, recursiveFloodFillData, recursiveFloodFillData.RecursiveItems.Item2);
             
             if (recursiveItems.Item1 != null && recursiveItems.Item1.Count > minRoomSize) RecursiveFloodFill(newRecursiveFloodFillData.Item1);
             if (recursiveItems.Item2 != null && recursiveItems.Item2.Count > minRoomSize) RecursiveFloodFill(newRecursiveFloodFillData.Item2);
         }
-        private static RecursiveFloodFillData NewRecursiveFloodFillData(RecursiveFloodFillData data, HashSet<TileSystem.Tile> recursiveItems)
+        private static RecursiveFloodFillData NewRecursiveFloodFillData(TileMapClass tileMap, RecursiveFloodFillData data, HashSet<Tile> recursiveItems)
         {
             RecursiveFloodFillData newRecursiveFloodFillData = new ((null, null), data.MinRoomSize);
             
             if (recursiveItems == null) return newRecursiveFloodFillData;
             if (recursiveItems.Count <= data.MinRoomSize) return newRecursiveFloodFillData;
             
-            (HashSet<Tile>, HashSet<Tile>) newRecursiveItems = FloodFill(ResetTilesAndGetSeedTile(recursiveItems, _tileMap));
+            (HashSet<Tile>, HashSet<Tile>) newRecursiveItems = FloodFill(ResetTilesAndGetSeedTile(recursiveItems, tileMap));
             
             newRecursiveFloodFillData = new RecursiveFloodFillData(newRecursiveItems, data.MinRoomSize);
             return newRecursiveFloodFillData;
