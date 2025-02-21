9ffc92818e468e925622e0abac209b5b8ff4671e
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-02-21 14:16:41 +0100

    Fixs a MeshHanler Null Refrens Eror

--- Assets/Scripts/MeshHandlers/MeshHandler.cs
+++ Assets/Scripts/MeshHandlers/MeshHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace MeshHandlers
@@ -8,6 +9,12 @@ namespace MeshHandlers
     { 
         protected MeshFilter MeshFilter { get; private set; }
         protected MeshRenderer MeshRenderer { get; private set; }
+
+        private void OnValidate()
+        {
+            if (MeshFilter == null) MeshFilter = gameObject.GetComponent<MeshFilter>(); 
+            if (MeshRenderer == null) MeshRenderer = gameObject.GetComponent<MeshRenderer>(); 
+        }
 
         private void OnEnable() => (MeshFilter, MeshRenderer) = (gameObject.GetComponent<MeshFilter>(), gameObject.GetComponent<MeshRenderer>());
         private void OnDisable() => (MeshFilter, MeshRenderer) = (null, null);
