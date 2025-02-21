401076e3053b4515f203462a5e5a96104caaeb16
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-02-21 14:23:49 +0100

    fixt DiesFunction to bi static

--- Assets/Scripts/Dies/DiesFunction.cs
+++ Assets/Scripts/Dies/DiesFunction.cs
@@ -3,7 +3,7 @@ using Random = UnityEngine.Random;
 
 namespace Dies
 {
-    public abstract class DiesFunction
+    public static class DiesFunction
     {
         public static int RollADie(DiesEnum dies) => Random.Range(0, (int)dies) + 1;
         public static int RollDies(DiesEnum[] dies) => dies.Sum(RollADie);
