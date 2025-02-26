0a9fdd3c3d3277cbb77695d100bdc29bfffd9c22
Emanuel Dahlan <Dahlin.emanuel@gmail.com>
2025-02-26 11:21:17 +0100

    add Base UnitAction

---
+++ Assets/Scripts/Events/Action/UnitAction.cs
@@ -0,0 +1,11 @@
+using Game.UnitClasses;
+
+namespace Events.Action
+{
+    public abstract class UnitAction : GameEvent
+    {
+        protected Unit Unit;
+        protected UnitAction(Unit unit) => Unit = unit;
+        public override string ToString() => $"Unit {GetType().Name} Input Event: {Unit.name}";
+    }
+}
